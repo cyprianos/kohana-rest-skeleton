17b6127d45b0ef280908769ad327b0b242d5ed17
Cyprian Gepfert cgepfert@gmail.com <cgepfert@gmail.com>
2015-03-10 19:28:22 +0100

    add movie via form & POST

--- app/application/classes/Controller/Api/Movie.php
+++ app/application/classes/Controller/Api/Movie.php
@@ -5,13 +5,27 @@ class Controller_Api_Movie extends Controller {
 	public function action_get()
 	{
 		$ret = ORM::collection(ORM::factory('Movie'));
+		
 		$ret = json_encode($ret);
+		
 		$this->response->body($ret);
-		//array('records'=>$ret)
+	
 	}
 	public function action_post()
 	{
-		$this->response->body('POST');
+		
+		$movie = new Model_Movie();
+		$movie->title = $this->request->post('title');
+		$movie->year = intval($this->request->post('year'));
+		$movie->ip = Request::$client_ip;
+		
+		$movie->save();
+		
+		$ret = json_encode($movie->toObj());
+
+		$this->response->body($ret);
+		
+		
 	}
 	public function action_delete()
 	{
